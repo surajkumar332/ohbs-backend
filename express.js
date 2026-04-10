e0ca5e956c3cce4de9fb663a576683c4b9fe3a15
Suraj Kumar332 <surajk71621@gmail.com>
2026-04-10 20:45:21 +0530

    cors issue

--- express.js
+++ express.js
@@ -14,7 +14,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Db connect
