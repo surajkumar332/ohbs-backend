d45fa260c654a2bc91fa620d538ffbae3cd192f8
Suraj Kumar332 <surajk71621@gmail.com>
2026-04-10 12:30:40 +0530

    fix auth middleware undefined error

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -1,13 +1,17 @@
 import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
-    const header = req.headers?.authorization;
+    const authHeader = req.headers?.authorization;
 
-    if (!header) {
-        return res.status(401).json({ message: "no token provided" });
+    if (!authHeader) {
+        return res.status(401).json({ message: "No token provided" });
     }
 
-    const token = header.split(" ")[1];
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Invalid token format" });
+    }
 
     try {
         const decode = jwt.verify(token, process.env.JWT_SECRETKEY);
