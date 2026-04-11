80db1d78a32e34f74c35703ba300bbd1b314253a
Suraj Kumar332 <surajk71621@gmail.com>
2026-04-11 18:53:42 +0530

    debug cors proof

--- express.js
+++ express.js
@@ -15,14 +15,18 @@ dotenv.config();
 
 const app = express();
 
-// ⭐ FORCE CORS (no library)
+console.log("=== NEW BUILD RUNNING ===");
+
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  console.log("CORS MIDDLEWARE HIT:", req.method, req.url);
+
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
+    return res.status(200).end();
   }
 
   next();
