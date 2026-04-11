1190d01fd59349b8325e943f9f7a02fd713d38ed
Suraj Kumar332 <surajk71621@gmail.com>
2026-04-11 18:51:50 +0530

    hard cors fix

--- express.js
+++ express.js
@@ -14,8 +14,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
+// ⭐ FORCE CORS (no library)
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
